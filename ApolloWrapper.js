bf04ca7d36f008918691cbec2f3154472939259d
mwaseem-js <mwaseem.js@gmail.com>
2024-10-06 12:20:20 +0530

    Add files via upload

---
+++ ApolloWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './client';
+
+const ApolloWrapper = ({ children }) => {
+  return (
+    <ApolloProvider client={client}>
+      {children}
+    </ApolloProvider>
+  );
+};
+
+export default ApolloWrapper;
